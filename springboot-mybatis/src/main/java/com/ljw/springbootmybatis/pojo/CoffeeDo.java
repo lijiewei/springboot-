5204ad6fda63da59f3e999d4cdcedead25f86dd1
lijiewei <lijiewei@github.com>
2019-08-29 08:37:19 +0800

    修改实体

--- springboot-mybatis/src/main/java/com/ljw/springbootmybatis/pojo/CoffeeDo.java
+++ springboot-mybatis/src/main/java/com/ljw/springbootmybatis/pojo/CoffeeDo.java
@@ -1,9 +1,26 @@
 package com.ljw.springbootmybatis.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.joda.money.Money;
+
+import java.util.Date;
+
 /**
  * @Description:
  * @Author: Administrator
  * @CreateDate: 2019/8/29 8:15
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class CoffeeDo {
+    private Long id;
+    private String name;
+    private Money price;
+    private Date createTime;
+    private Date updateTime;
 }
